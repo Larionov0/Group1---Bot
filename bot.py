7cff1b50c641d995a75ecc5ee980e1c075808a57
Larko_0 <larionov1001@gmail.com>
2021-01-24 15:19:38 +0200

    User class added

--- bot.py
+++ bot.py
@@ -13,11 +13,17 @@ TOKEN = '1583405375:AAF3aRNGS_-ksh_cuOeHddQOonlwPZ2F-bA'
 URL = f"{URL_BASE}/bot{TOKEN}"
 
 
+class User:
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+
+
 class Bot:
     def __init__(self, token):
         self.token = token
         self.last_update_id = 0
         self.url = f"{URL_BASE}/bot{token}"
+        self.users = []
 
     def get_updates(self):
         response = requests.get(f"{self.url}/getUpdates?offset={self.last_update_id + 1}")
@@ -31,19 +37,32 @@ class Bot:
         while True:
             updates = self.get_updates()
             for update in updates:
-                self.answer_to_update(update)
+                user = self.identify_user(update)
+                text: str = update['message']['text']
+                self.answer_to_message(user, text)
                 self.last_update_id = update['update_id']
             time.sleep(0.5)
 
-    def answer_to_update(self, update):
+    def identify_user(self, update):
         chat_id = update['message']['chat']['id']
-        text: str = update['message']['text']
+        user = self.find_user(chat_id)
+        if user is None:
+            user = self.create_user(chat_id)
+            self.users.append(user)
+        return user
 
-        if 'как дела' in text.lower():
-            answer = random.choices(['Хорошо', "Отлично", "Не очень", "Плохо", "Хорошечно"], [5, 2, 4, 1, 1])[0]
-        else:
-            answer = text + '!'
-        self.send_message(chat_id, answer)
+    def find_user(self, chat_id):
+        for user in self.users:
+            if user.chat_id == chat_id:
+                return user
+        return None
+
+    def create_user(self, chat_id):
+        user = User(chat_id)
+        return user
+
+    def answer_to_message(self, user, text):
+        self.send_message(user.chat_id, 'Пока в разработке')
 
 
 bot = Bot(TOKEN)
